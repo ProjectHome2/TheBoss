81d9e07fa1cce79e1808872c96b052b03b34b5d6
Andreas Hald Espensen <Andreas Hald Espensen>
2016-10-06 12:32:18 +0200

    opdateret script

--- guf.R
+++ guf.R
@@ -1,8 +1,67 @@
-load("C:/Users/Andreas/Desktop/UNi/5. semester/Data/home.Rdata") 
-home = HOME2
-library("ggplot2")
-plot(home[,6],home[,5])
-ggplot(home, aes(x=as.factor(Postnr), y=as.integer(Liggetid), colour = Boligtilstand)) + geom_point()
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+load("homeFull.Rdata") 
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
 
-ggplot(home, aes(x=as.integer(Boligareal), y=as.integer(Kontantpris), colour = Boligtilstand)) + geom_point()
+#str() : Gives information about the data e.g 
+
+ggplot(home, aes(x=PostalCode, y=SalesPeriod, colour = Condition)) + geom_point()
+
+names(home)
+#These are linear
+ggplot(home, aes(x=LivingArea, y=Price)) + geom_point()
+ggplot(home, aes(x=Age, y=Price)) + geom_point() #Might be good
+
+#Might be linear
+ggplot(home, aes(x=SalesPeriod, y=Price)) + geom_point() #Make a bar plot maybe
+ggplot(home, aes(x=BasementArea, y=Price)) + geom_point()
+ggplot(home, aes(x=GroundArea, y=Price)) + geom_point()
+ggplot(home, aes(x=ConstructionYear, y=Price)) + geom_point()
+
+#Makes no sense to check for linear dependence
+ggplot(home, aes(x=ManyToilets, y=Price)) + geom_point()
+ggplot(home, aes(x=Basement, y=Price)) + geom_point()
+ggplot(home, aes(x=LargeGround, y=Price)) + geom_point()
+ggplot(home, aes(x=Renovation, y=Price)) + geom_point()
+ggplot(home, aes(x=HighHouse, y=Price)) + geom_point()
+ggplot(home, aes(x=Quarter, y=Price)) + geom_point()
+ggplot(home, aes(x=Levels, y=Price)) + geom_point()
+ggplot(home, aes(x=NumberOfToilets, y=Price)) + geom_point()
+ggplot(home, aes(x=NewKitchen, y=Price)) + geom_point()
+ggplot(home, aes(x=NewWindows, y=Price)) + geom_point()
+ggplot(home, aes(x=NewBathroom, y=Price)) + geom_point()
+ggplot(home, aes(x=as.factor(NumberOfBedrooms), y=Price)) + geom_point() #x is a int therefore we need to use it as a factor to show th NA.
+ggplot(home, aes(x=Balcony, y=Price)) + geom_point()
+ggplot(home, aes(x=CloseToWater, y=Price)) + geom_point()
+ggplot(home, aes(x=Garage, y=Price)) + geom_point()
+ggplot(home, aes(x=YearOfSale, y=Price)) + geom_point()
+ggplot(home, aes(x=NumberOfBedrooms, y=Price, colour = Condition)) + geom_point() #Might be linear
+
+#Checking the Histograms
+ggplot(home, aes(x=NewKitchen)) + geom_bar(stat = "count")
+ggplot(home, aes(x=NewWindows)) + geom_bar(stat = "count")
+ggplot(home, aes(x=CloseToWater)) + geom_bar(stat = "count")
+ggplot(home, aes(x=NewBathroom)) + geom_bar(stat = "count")
+ggplot(home, aes(x=as.factor(NumberOfToilets))) + geom_bar(stat = "count") + labs(x = "NumberOfToilets")
+ggplot(home, aes(x=as.factor(NumberOfBedrooms))) + geom_bar(stat = "count") + labs(x = "NumberOfBedrooms")
+ggplot(home, aes(x=ManyToilets)) + geom_bar(stat = "count")
+ggplot(home, aes(x=Garage)) + geom_bar(stat = "count")
+ggplot(home, aes(x=Renovation)) + geom_bar(stat = "count")
+ggplot(home, aes(x=Basement)) + geom_bar(stat = "count")
+ggplot(home, aes(x=LargeGround)) + geom_bar(stat = "count")
+ggplot(home, aes(x=HighHouse)) + geom_bar(stat = "count")
+ggplot(home, aes(x=Quarter)) + geom_bar(stat = "count")
+ggplot(home, aes(x=as.factor(Levels))) + geom_bar(stat = "count") + labs(x = "Levels")
+ggplot(home, aes(x=Balcony)) + geom_bar(stat = "count")
+ggplot(home, aes(x=YearOfSale)) + geom_bar(stat = "count")
+
+
+
+
+
+ggplot(home, aes(x=as.factor(NumberOfBedrooms), y=Price)) + geom_point() #x is a int therefore we need to use it as a factor to show th NA.
+
+ggplot(home, aes(x=Price, fill = as.factor(Levels))) + geom_histogram(bins = 100) + geom_vline(xintercept = mean(home$Price))
+
 
